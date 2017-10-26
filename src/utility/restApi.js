d401c99d64c52409b389dee245e670cc82516f78
John Livingston <jmlivingston@gmail.com>
2017-10-26 10:29:39 -0700

    Add login-key Add debug npm script

--- src/utility/restApi.js
+++ src/utility/restApi.js
@@ -1,19 +1,23 @@
-const fetch = require('node-fetch');
+const fetch = require('node-fetch')
 
-const headers = { 'Content-Type': 'application/json; charset=utf-8', 'gg-client-api-key': 'C434EDE3-2E7E-4B9D-A070-58B2CF94846D' }
+const headers = {
+  'Content-Type': 'application/json; charset=utf-8',
+  'gg-client-api-key': 'C434EDE3-2E7E-4B9D-A070-58B2CF94846D',
+  'gg-user-login-key': '497b289d-76ee-4215-b9a0-c2fc547b3240'
+}
 
 module.exports = {
   GET: async ({ url }) => {
     const res = await fetch(url, { method: 'GET', headers })
     const json = await res.json()
-    return res.status === 404 ? null : json // TODO: Add better error handling    
+    return res.status === 404 ? null : json // TODO: Add better error handling
   },
   POST: ({ url, body }) => {
     return fetch(url, {
       method: 'POST',
       headers,
       body: JSON.stringify(body)
-    }).then(res => {      
+    }).then(res => {
       return res.json()
     })
   },
